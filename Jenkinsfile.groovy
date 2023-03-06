8aca1a66a90e89d478dc1e12b9d8b7920810db59
Nikolay Asenov <nikolay.asenov@cleverpine.com>
2023-03-06 15:22:35 +0200

    add lht registry as env var

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -32,7 +32,7 @@ pipeline {
       }
 
       environment {
-          // NPM_CONFIG_REGISTRY = 'https://lht.app.lufthansa.com/artifactory/api/npm/npm-local/'
+          NPM_CONFIG_REGISTRY = 'https://lht.app.lufthansa.com/artifactory/api/npm/npm-local/'
           NPM_CONFIG__AUTH = credentials('npm-publish-token')
           NPM_CONFIG_ALWAYS_AUTH = true
           NPM_CONFIG_EMAIL = 'hamtiebs@lht.dlh.de'
